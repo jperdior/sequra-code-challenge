14baa8b5498455d98dad16b577d58a0cd8678934
Julio Perdiguer <julio.perdiguer.extern@internetstores.com>
2023-12-01 15:55:23 +0100

    fixtures working

--- code/backend/src/DataFixtures/PurchasesFixtures.php
+++ code/backend/src/DataFixtures/PurchasesFixtures.php
@@ -9,47 +9,46 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class PurchasesFixtures extends Fixture
+class PurchasesFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
 
-//        $purchasesCsvPath = __DIR__ . '/orders.csv';
-//        $purchasesCsv = fopen($purchasesCsvPath, 'r');
-//
-//        //skip first line
-//        fgetcsv($purchasesCsv);
-//
-//        $batchSize = 20;
-//        $i = 0;
-//
-//        $manager->getConnection()->getConfiguration()->setSQLLogger(null);
-//
-//        while (($purchasesCsvRow = fgetcsv($purchasesCsv, null, ';')) !== false) {
-//            $purchase = new Purchase();
-//            $purchase->setId($purchasesCsvRow[0]);
-//            $merchant = $manager->getRepository(Merchant::class)->findOneBy(['reference' => $purchasesCsvRow[1]]);
-//            $purchase->setMerchant($merchant);
-//            $purchase->setAmount(floatval($purchasesCsvRow[2]));
-//            $purchase->setCreatedAt(new \DateTime($purchasesCsvRow[3]));
-//            $purchase->setStatus(PurchaseStatusEnum::PENDING->value);
-//            $manager->persist($purchase);
-//            $manager->detach($purchase);
-//            if ($i % $batchSize === 0) {
-//                $manager->flush();
-//                $manager->clear();
-//            }
-//            $i++;
-//        }
-//
-//        $manager->flush();
-//        $manager->clear();
+        $purchasesCsvPath = __DIR__ . '/orders.csv';
+        $purchasesCsv = fopen($purchasesCsvPath, 'r');
+
+        //skip first line
+        fgetcsv($purchasesCsv);
+
+        $batchSize = 20;
+        $i = 0;
+
+        $manager->getConnection()->getConfiguration()->setSQLLogger(null);
+
+        while (($purchasesCsvRow = fgetcsv($purchasesCsv, null, ';')) !== false) {
+            $purchase = new Purchase();
+            $purchase->setId($purchasesCsvRow[0]);
+            $merchant = $manager->getRepository(Merchant::class)->findOneBy(['reference' => $purchasesCsvRow[1]]);
+            $purchase->setMerchant($merchant);
+            $purchase->setAmount(floatval($purchasesCsvRow[2]));
+            $purchase->setCreatedAt(new \DateTime($purchasesCsvRow[3]));
+            $purchase->setStatus(PurchaseStatusEnum::PENDING->value);
+            $manager->persist($purchase);
+            if ($i % $batchSize === 0) {
+                $manager->flush();
+                $manager->clear();
+            }
+            $i++;
+        }
+
+        $manager->flush();
+        $manager->clear();
     }
 
-//    public function getDependencies(): array
-//    {
-//        return [
-//            MerchantFixtures::class,
-//        ];
-//    }
+    public function getDependencies(): array
+    {
+        return [
+            MerchantFixtures::class,
+        ];
+    }
 }
