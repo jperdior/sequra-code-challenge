ebedfb174f6e4285bb9ff366850452dab2b3dd50
Julio Perdiguer <julio.perdiguer.extern@internetstores.com>
2023-12-29 22:29:02 +0100

    making disbursement line an aggregate root

--- code/backend/src/SequraChallenge/DisbursementLines/Domain/Entity/DisbursementLine.php
+++ code/backend/src/SequraChallenge/DisbursementLines/Domain/Entity/DisbursementLine.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 namespace App\SequraChallenge\DisbursementLines\Domain\Entity;
 
 use App\SequraChallenge\Shared\Domain\Disbursements\DisbursementReference;
+use App\Shared\Domain\Aggregate\AggregateRoot;
 
-readonly class DisbursementLine
+class DisbursementLine extends AggregateRoot
 {
 
     public function __construct(
-        public DisbursementLineId             $id,
-        public DisbursementReference          $disbursementReference,
-        public DisbursementLinePurchaseId     $purchaseId,
-        public DisbursementLinePurchaseAmount $purchaseAmount,
-        public DisbursementLineAmount         $amount,
-        public DisbursementLineFeePercentage  $feePercentage,
-        public DisbursementLineFeeAmount      $feeAmount,
-        public \DateTime                      $createdAt = new \DateTime(),
+        public readonly DisbursementLineId             $id,
+        public readonly DisbursementReference          $disbursementReference,
+        public readonly DisbursementLinePurchaseId     $purchaseId,
+        public readonly DisbursementLinePurchaseAmount $purchaseAmount,
+        public readonly DisbursementLineAmount         $amount,
+        public readonly DisbursementLineFeePercentage  $feePercentage,
+        public readonly DisbursementLineFeeAmount      $feeAmount,
+        public readonly \DateTime                      $createdAt = new \DateTime(),
     )
     {
     }
